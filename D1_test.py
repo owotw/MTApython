1571357dd5c468d6923a19ed4ae5ce6a08715849
owotw <78326095+owotw@users.noreply.github.com>
2021-02-01 10:42:29 +0800

    Add files via upload

---
+++ D1_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 09:28:34 2021
+
+@author: MTAEXAM
+"""
+
+top = float(input("上底:"))
+bottom = float(input("下底:"))
+height = float(input("高:"))
+
+area = (top + bottom) * height / 2
+
+print("梯形面積:" + str(area))
+
+
+
